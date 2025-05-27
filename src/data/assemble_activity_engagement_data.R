151d8c763b7697a313ef7f7a782ce2c3afd12092
Jennifer Fedor <fedorjm@gmail.com>
2025-05-27 18:31:36 -0400

    Update activity engagement

---
+++ src/data/assemble_activity_engagement_data.R
@@ -0,0 +1,270 @@
+# Assemble activity engagement (selection, phase 1 endorsement, and phase 2
+# planning and completion) data
+
+library(tidyverse)
+
+
+#### helper functions ----
+
+format_activity_selections <- function(df, phase) {
+  df <- df %>%
+    dplyr::select(-ends_with("date")) %>%
+    rowwise() %>%
+    mutate(
+      activity_type = ifelse(
+        grepl(glue::glue("^{pId}"), activityId),
+        "Custom",
+        "Default"
+      ),
+      !!glue::glue("selected_phase{phase}") := 1
+    ) %>%
+    ungroup() %>%
+    rename(
+      record_id = pId,
+      activity_id = activityId
+    ) 
+  return(df)
+}
+
+
+#### settings ----
+
+parent_file_path <- "balance-quantitative"
+input_file_path <- "data/interim"
+output_file_path <- "data/processed"
+output_file_name <- "balance_activity_engagement.csv"
+
+
+#### activity selections ----
+
+selected_phase1 <- read_csv(here::here(parent_file_path, input_file_path, "balance_phase1_selected_activities.csv"))
+selected_phase2 <- read_csv(here::here(parent_file_path, input_file_path, "balance_phase2_selected_activities.csv"))
+
+selected <- format_activity_selections(selected_phase1, 1) %>%
+  full_join(
+    format_activity_selections(selected_phase2, 2),
+    by = c("record_id", "activity_id", "activity_name", "activity_type")
+  ) %>%
+  replace_na(
+    list(
+      selected_phase1 = 0,
+      selected_phase2 = 0
+    )
+  )
+
+activity_selection <- selected %>%
+  summarize(
+    .by = c(record_id, activity_type),
+    phase1 = sum(selected_phase1),
+    phase2 = sum(selected_phase2),
+    phase1_and_phase2 = sum(selected_phase1 == 1 & selected_phase2 == 1),
+    phase1_not_phase2 = sum(selected_phase1 == 1 & selected_phase2 == 0),
+    phase2_not_phase1 = sum(selected_phase1 == 0 & selected_phase2 == 1)
+  ) %>%
+  bind_rows(
+    selected %>%
+      summarize(
+        .by = record_id,
+        phase1 = sum(selected_phase1),
+        phase2 = sum(selected_phase2),
+        phase1_and_phase2 = sum(selected_phase1 == 1 & selected_phase2 == 1),
+        phase1_not_phase2 = sum(selected_phase1 == 1 & selected_phase2 == 0),
+        phase2_not_phase1 = sum(selected_phase1 == 0 & selected_phase2 == 1)
+      ) %>%
+      mutate(activity_type = "All")
+  ) %>%
+  arrange(record_id, activity_type) %>%
+  pivot_wider(
+    names_from = activity_type, 
+    values_from = starts_with("phase"),
+    names_glue = "selected_{.value}_{tolower(activity_type)}"
+  )
+
+
+#### phase 1 activity endorsements ----
+
+endorsed <- read_csv(here::here(parent_file_path, input_file_path, "balance_phase1_endorsed_activities.csv"))
+
+endorsed <- endorsed %>%
+  rename(
+    record_id = pId,
+    activity_id = activityId
+  )
+
+activity_endorsement_participant_level <- endorsed %>%
+  summarize(
+    .by = record_id,
+    daily_surveys = length(unique(survey_id_daily)),
+    total_endorsed = sum(endorsed),
+    total_rated = sum(!is.na(activity_rating)),
+    unique_endorsed = length(unique(activity_id)),
+    unique_rated = length(unique(activity_id[!is.na(activity_rating)])),
+    across(
+      .cols = c("activity_rating"),
+      .fns = list(
+        "mean" = function(x) mean(x, na.rm = TRUE),
+        "sd" = function(x) sd(x, na.rm = TRUE),
+        "min" = function(x) min(x, na.rm = TRUE),
+        "max" = function(x) max(x, na.rm = TRUE)
+      )
+    )
+  ) 
+
+activity_endorsement_day_level <- endorsed %>%
+  summarize(
+    .by = c(record_id, date),
+    total_endorsed = sum(endorsed),
+    total_rated = sum(!is.na(activity_rating)),
+    across(
+      .cols = c("activity_rating"),
+      .fns = list(
+        "mean" = function(x) mean(x, na.rm = TRUE),
+        "min" = function(x) min(x, na.rm = TRUE),
+        "max" = function(x) max(x, na.rm = TRUE)
+      )
+    )
+  ) %>%
+  summarize(
+    .by = record_id,
+    across(
+      .cols = where(is.numeric),
+      .fns = list(
+        "mean" = function(x) mean(x, na.rm = TRUE),
+        "sd" = function(x) sd(x, na.rm = TRUE),
+        "min" = function(x) min(x, na.rm = TRUE),
+        "max" = function(x) max(x, na.rm = TRUE)
+      )
+    )
+  )
+
+activity_endorsement <- activity_endorsement_participant_level %>%
+  full_join(activity_endorsement_day_level, by = c("record_id")) %>%
+  rename_all(
+    function(x) ifelse(!grepl("record_id", x), paste0("phase1_", x), x) 
+  )
+
+
+#### phase 2 activity planning and completion ----
+
+planned_morning <- read_csv(here::here(parent_file_path, input_file_path, "balance_phase2_planned_activities.csv"))
+completed_evening <- read_csv(here::here(parent_file_path, input_file_path, "balance_phase2_completed_activities.csv"))
+
+planned_and_completed <- planned_morning %>%
+  full_join(
+    completed_evening, 
+    by = c("pId", "date", "activityId")
+  ) %>%
+  full_join(
+    planned_morning %>%
+      select(pId, date) %>%
+      distinct() %>%
+      mutate(has_morning = 1),
+    by = c("pId", "date")
+  ) %>%
+  full_join(
+    completed_evening %>%
+      select(pId, date) %>%
+      distinct() %>%
+      mutate(has_evening = 1),
+    by = c("pId", "date")
+  ) %>%
+  replace_na(
+    list(
+      has_morning = 0,
+      has_evening = 0
+    )
+  ) %>%
+  select(
+    record_id = pId, 
+    date,
+    has_morning,
+    has_evening,
+    activity_id = activityId,
+    planned,
+    completed 
+  ) %>%
+  arrange(record_id, date, activity_id) %>%
+  mutate(
+    planned = case_when(
+      has_morning == 1 & is.na(planned) ~ 0,
+      TRUE ~ planned
+    ),
+    completed = case_when(
+      has_evening == 1 & is.na(completed) ~ 0,
+      TRUE ~ completed
+    )
+  )
+
+activity_completion_participant_level <- planned_and_completed %>%
+  summarize(
+    .by = record_id,
+    # data quantity
+    morning_surveys = length(unique(date[has_morning == 1])),
+    evening_surveys = length(unique(date[has_evening == 1])),
+    morning_and_evening_surveys = length(unique(date[has_morning == 1 & has_evening == 1])),
+    # total
+    total_planned = sum(planned == 1, na.rm = TRUE),
+    total_completed = sum(completed == 1, na.rm = TRUE),
+    total_planned_and_completed = sum(planned == 1 & completed == 1, na.rm = TRUE),
+    total_planned_not_completed = sum(planned == 1 & completed == 0, na.rm = TRUE),
+    total_completed_not_planned = sum(planned == 0 & completed == 1, na.rm = TRUE),
+    total_not_planned_not_completed = sum(planned == 0 & completed == 0, na.rm = TRUE),
+    # unique
+    unique_planned = length(na.omit(unique(activity_id[planned == 1]))),
+    unique_completed = length(na.omit(unique(activity_id[completed == 1]))),
+    unique_planned_and_completed = length(na.omit(unique(activity_id[planned == 1 & completed == 1]))),
+    unique_planned_not_completed = length(na.omit(unique(activity_id[planned == 1 & completed == 0]))),
+    unique_completed_not_planned = length(na.omit(unique(activity_id[planned == 0 & completed == 1]))),
+    # list of unique
+    unique_planned_list = paste0(sort(na.omit(unique(activity_id[planned == 1]))), collapse=", "),
+    unique_completed_list = paste0(sort(na.omit(unique(activity_id[completed == 1]))), collapse=", "),
+    unique_planned_and_completed_list = paste0(sort(na.omit(unique(activity_id[planned == 1 & completed == 1]))), collapse=", "),
+    unique_planned_not_completed_list = paste0(sort(na.omit(unique(activity_id[planned == 1 & completed == 0]))), collapse=", "),
+    unique_completed_not_planned_list = paste0(sort(na.omit(unique(activity_id[planned == 0 & completed == 1]))), collapse=", ")
+  ) 
+
+activity_completion_day_level <- planned_and_completed %>%
+  summarize(
+    .by = c(record_id, date, has_morning, has_evening),
+    total_planned = sum(planned == 1, na.rm = TRUE),
+    total_completed = sum(completed == 1, na.rm = TRUE),
+    total_planned_and_completed = sum(planned == 1 & completed == 1, na.rm = TRUE)
+  ) %>%
+  mutate(
+    planned_activity_completion_rate = case_when(
+      has_morning == 1 & has_evening == 1 ~ total_planned_and_completed / total_planned,
+      TRUE ~ NA
+    )
+  ) %>%
+  summarize(
+    .by = record_id,
+    across(
+      .cols = ends_with("rate"),
+      .fns = list(
+        "n" = function(x) sum(!is.na(x)),
+        "mean" = function(x) mean(x, na.rm = TRUE),
+        "sd" = function(x) sd(x, na.rm = TRUE),
+        "min" = function(x) min(x, na.rm = TRUE),
+        "max" = function(x) max(x, na.rm = TRUE)
+      )
+    )
+  )
+
+activity_completion <- activity_completion_participant_level %>%
+  full_join(
+    activity_completion_day_level,
+    by = c("record_id")
+  ) %>%
+  rename_all(
+    function(x) ifelse(!grepl("record_id", x), paste0("phase2_", x), x) 
+  )
+
+
+#### combine and save output ----
+
+activity_engagement <- activity_selection %>%
+  full_join(activity_endorsement, by = c("record_id")) %>%
+  full_join(activity_completion, by = c("record_id"))
+
+write_csv(activity_engagement, here::here(parent_file_path, output_file_path, output_file_name))
+
--- src/data/pull_activity_endorsements.py
+++ src/data/pull_activity_endorsements.py
@@ -4,90 +4,150 @@ import numpy as np
 from utils import load_credentials, connect_to_database
 
 def main():
-    derived_tables_prefix = f""" 
+    derived_tables = """
     with 
-        phase1_dates as (
-            select pId, startDate as phase1_start_date, date_sub(endDate, interval 1 day) as phase1_end_date
-            from user_study_phases
-            where phaseId = 'PHASE_1' and pId like '1%' and pId != '121'
-        ),
-        phase2_dates as (
-            select pId, startDate as phase2_start_date, date_sub(endDate, interval 1 day) as phase2_end_date
-            from user_study_phases
-            where phaseId = 'PHASE_2' and pId like '1%' and pId != '121'
-        ),
         activity_id_to_name as (
-            select activityId, name as activity_name, 1 as custom_activity
-            from user_activities
-            union all
-            select activityId, name as activity_name, 0 as custom_activity
+            select distinct activityId, name as activity_name
             from activities
+            union all 
+            select distinct activityId, name as activity_name
+            from user_activities
+            order by activityId
         ),
-        pid_activity_preferences as (
-            select pId, substring(participantPhaseId, -1) as study_phase, activityId
-            from user_activity_preferences
-        ),
-        pid_activity_endorsements as (
+        phase1_dates as (
             select 
                 pId, 
-                date, 
-                sId as survey_type, 
-                survey_response_details.surveyId, 
-                goodnessScore as goodness_score, 
-                activityId, 
-                score as activity_score
-            from survey_response_details
-            join survey_responses on survey_response_details.surveyId = survey_responses.surveyId
+                startDate as phase1_start_date, 
+                date_sub(endDate, interval 1 day) as phase1_end_date
+            from user_study_phases
+            where 
+                phaseId = 'PHASE_1' and 
+                pId like '1%' and 
+                pId != '121'
+            order by pId
+        ),
+        phase1_selected_activities as (
+            select distinct
+                user_activity_preferences.pId,
+                phase1_start_date,
+                phase1_end_date,
+                user_activity_preferences.activityId,
+                activity_name
+            from user_activity_preferences
+            left join activity_id_to_name on user_activity_preferences.activityId = activity_id_to_name.activityId
+            left join phase1_dates on user_activity_preferences.pId = phase1_dates.pId
+            where 
+                participantPhaseId like "%_PHASE_1" and
+                user_activity_preferences.pId like '1%' and 
+                user_activity_preferences.pId != '121'
+            order by pId, activityId
+        ),
+        phase1_endorsed_activities as (
+            select distinct
+                survey_response_details.surveyId as survey_id_daily,
+                survey_responses.pId,
+                date,
+                activityId,
+                1 as endorsed,
+                case  
+                    when score = -1 then null
+                    else score 
+                end as activity_rating
+            from survey_response_details 
+            left join survey_responses on survey_response_details.surveyId = survey_responses.surveyId
+            left join phase1_dates on survey_responses.pId = phase1_dates.pId
+            where 
+                date >= phase1_start_date and
+                date <= phase1_end_date and
+                survey_response_details.surveyId like '%_DAILY_%' and 
+                survey_responses.pId like '1%' and 
+                survey_responses.pId != '121'
+            order by pId, date, activityId
+        ),
+        phase2_dates as (
+            select 
+                pId, 
+                startDate as phase2_start_date, 
+                date_sub(endDate, interval 1 day) as phase2_end_date
+            from user_study_phases
+            where 
+                phaseId = 'PHASE_2' and 
+                pId like '1%' and 
+                pId != '121'
+            order by pId
+        ),
+        phase2_selected_activities as (
+            select distinct
+                user_activity_preferences.pId,
+                phase2_start_date,
+                phase2_end_date,
+                user_activity_preferences.activityId,
+                activity_name
+            from user_activity_preferences
+            left join activity_id_to_name on user_activity_preferences.activityId = activity_id_to_name.activityId
+            left join phase2_dates on user_activity_preferences.pId = phase2_dates.pId
+            where 
+                participantPhaseId like "%_PHASE_2" and
+                user_activity_preferences.pId like '1%' and 
+                user_activity_preferences.pId != '121'
+            order by pId, activityId
+        ),
+        phase2_planned_activities as (
+            select distinct
+                survey_response_details.surveyId as survey_id_morning,
+                survey_responses.pId,
+                date,
+                activityId,
+                1 as planned
+            from survey_response_details 
+            left join survey_responses on survey_response_details.surveyId = survey_responses.surveyId
+            left join phase2_dates on survey_responses.pId = phase2_dates.pId
+            where 
+                date >= phase2_start_date and
+                date <= phase2_end_date and
+                survey_response_details.surveyId like '%_MORNING_%' and 
+                survey_responses.pId like '1%' and 
+                survey_responses.pId != '121'
+            order by pId, date, activityId
+        ),
+        phase2_completed_activities as (
+            select distinct
+                survey_response_details.surveyId as survey_id_evening,
+                survey_responses.pId,
+                date,
+                activityId,
+                1 as completed
+            from survey_response_details 
+            left join survey_responses on survey_response_details.surveyId = survey_responses.surveyId
+            left join phase2_dates on survey_responses.pId = phase2_dates.pId
+            where 
+                date >= phase2_start_date and
+                date <= phase2_end_date and
+                survey_response_details.surveyId like '%_EVENING_%' and 
+                survey_responses.pId like '1%' and 
+                survey_responses.pId != '121'
+            order by pId, date, activityId
         )
-    """
-
-    activity_preference_suffix = f""" 
-    select distinct
-        pId as record_id,
-        study_phase,
-        pid_activity_preferences.activityId as activity_id,
-        activity_name,
-        custom_activity
-    from pid_activity_preferences  
-    join activity_id_to_name on pid_activity_preferences.activityId = activity_id_to_name.activityId
-    where pId like '1%' and pId != '121'
-    order by record_id, study_phase, activity_id;    
-    """
-
-    activity_endorsement_suffix = f""" 
-    select 
-        pid_activity_endorsements.pId as record_id,
-        date,
-        case when survey_type = 'DAILY' then 1 else 2 end as study_phase,
-        survey_type,
-        surveyId as survey_id,
-        pid_activity_endorsements.activityId as activity_id,
-        activity_name,
-        custom_activity,
-        activity_score
-    from pid_activity_endorsements
-    join phase1_dates on pid_activity_endorsements.pId = phase1_dates.pId
-    join phase2_dates on pid_activity_endorsements.pId = phase2_dates.pId
-    join activity_id_to_name on pid_activity_endorsements.activityId = activity_id_to_name.activityId
-    where 
-        (survey_type = 'DAILY' and date >= phase1_start_date and date <= phase1_end_date) or 
-        (survey_type in ('MORNING', 'EVENING') and date >= phase2_start_date and date <= phase2_end_date)
-    order by record_id, date, survey_type desc;
     """
 
     output_file_path = "../../data/interim/"
 
+    tables_to_query = [
+        "phase1_selected_activities", 
+        "phase2_selected_activities", 
+        "phase1_endorsed_activities", 
+        "phase2_planned_activities", 
+        "phase2_completed_activities"
+    ]
+
     credentials = load_credentials(group="balance")
     con = connect_to_database(credentials)
 
-    activity_preference_query = derived_tables_prefix + activity_preference_suffix
-    activity_endorsement_query = derived_tables_prefix + activity_endorsement_suffix
-
-    activity_preference_data = pd.read_sql(sql=activity_preference_query, con=con)
-    activity_endorsement_data = pd.read_sql(sql=activity_endorsement_query, con=con)
-
-    activity_preference_data.to_csv(output_file_path + "balance_activity_preferences.csv", index=False)
-    activity_endorsement_data.to_csv(output_file_path + "balance_activity_endorsements.csv", index=False)
+    for table in tables_to_query:
+        output_file = f"{output_file_path}/balance_{table}.csv"
+        table_query = derived_tables + f"\nselect * from {table};"
+        table_data = pd.read_sql(table_query, con)
+        table_data.to_csv(output_file, index=False)
 
     con.close()
 
